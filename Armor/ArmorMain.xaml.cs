7928a0ef5ed846ee15669ecd2c3bd7218299ca02
Daniel <dholcomb1371@gmail.com>
2023-06-23 01:43:00 +0300

    FMA-8 Issue -> Completed list for Marauder's Helmet List Levels 1-10, copied from Warrior but removed one item as it wasn't for Marauder class. DJH

--- Armor/ArmorMain.xaml.cs
+++ Armor/ArmorMain.xaml.cs
@@ -16,4 +16,9 @@ public partial class ArmorMain : ContentPage
     {
         await Navigation.PushAsync(new Warrior.WarArm.WarriorArm());
     }
+
+    private async void Button_Clicked_2(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Maraudar.MarArm.MarArmMain());
+    }
 }
