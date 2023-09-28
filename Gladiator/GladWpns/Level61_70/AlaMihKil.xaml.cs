62aa72d1921da582bf8b18bd55037c42d1ab5ed7
Daniel <dholcomb1371@gmail.com>
2023-09-28 03:11:18 +0300

    FMA-18: Completed page creation and navigation to those pages.  There is an issue with workloads for 'maccatalyst', 'ios', and 'android'.  Need to keep an eye out when running testing at end of build epic. DJH

---
+++ Gladiator/GladWpns/Level61_70/AlaMihKil.xaml.cs
@@ -0,0 +1,9 @@
+namespace FinalFantasy14.Gladiator.GladWpns.Level61_70;
+
+public partial class AlaMihKil : ContentPage
+{
+	public AlaMihKil()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Gladiator/GladWpns/Level61_70/YamaBlade.xaml.cs
@@ -0,0 +1,9 @@
+namespace FinalFantasy14.Gladiator.GladWpns.Level61_70;
+
+public partial class YamaBlade : ContentPage
+{
+	public YamaBlade()
+	{
+		InitializeComponent();
+	}
+}
