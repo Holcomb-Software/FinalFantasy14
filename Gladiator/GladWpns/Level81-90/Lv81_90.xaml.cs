47f9f00c646bc2bbcf80737f482761c7ad031fc7
techsgtcarter99 <dholcomb1371@gmail.com>
2024-01-18 09:14:38 -0600

    FMA-20: Created pages and navigation for Levels 81-90 for the class of Gladiation. DJH

---
+++ Gladiator/GladWpns/Level81-90/Lv81_90.xaml.cs
@@ -0,0 +1,184 @@
+namespace FinalFantasy14.Gladiator.GladWpns.Level81_90;
+
+public partial class Lv81_90 : ContentPage
+{
+	public Lv81_90()
+	{
+		InitializeComponent();
+	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		await Navigation.PushAsync(new AbyssBstrdSwrd());
+    }
+
+    private async void Button_Clicked_1(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AsphLngswrd());
+    }
+
+    private async void Button_Clicked_2(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AugClssLngswrd());
+    }
+
+    private async void Button_Clicked_3(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AugDiaSwrd());
+    }
+
+    private async void Button_Clicked_4(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AugLunarEnvoyLngsSwrd());
+    }
+
+    private async void Button_Clicked_5(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AugPolPnt());
+    }
+
+    private async void Button_Clicked_6(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AugRadBstrdSwrd());
+    }
+
+    private async void Button_Clicked_7(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AugRinSwrd());
+    }
+
+    private async void Button_Clicked_8(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new BstrdSwrdDivLght());
+    }
+
+    private async void Button_Clicked_9(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new BisKatz());
+    }
+
+    private async void Button_Clicked_10(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new BluefeatherSwrd());
+    }
+
+    private async void Button_Clicked_11(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ChonMagSwrd());
+    }
+
+    private async void Button_Clicked_12(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ClassLngswrd());
+    }
+
+    private async void Button_Clicked_13(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new DiaSwrd());
+    }
+
+    private async void Button_Clicked_14(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new EnaKatz());
+    }
+
+    private async void Button_Clicked_15(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ExPolPnt());
+    }
+
+    private async void Button_Clicked_16(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new FlmclkdBrdswrd());
+    }
+
+    private async void Button_Clicked_17(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new HighDirLngswrd());
+    }
+
+    private async void Button_Clicked_18(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ImpMagSwrd());
+    }
+
+    private async void Button_Clicked_19(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new KitSwrd());
+    }
+
+    private async void Button_Clicked_20(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new LunarEnvLngswrd());
+    }
+
+    private async void Button_Clicked_21(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MangSwrd());
+    }
+
+    private async void Button_Clicked_22(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MonstFal());
+    }
+
+    private async void Button_Clicked_23(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MoonwardLngswrd());
+    }
+
+    private async void Button_Clicked_24(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new OrthKatz());
+    }
+
+    private async void Button_Clicked_25(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new PalaSham());
+    }
+
+    private async void Button_Clicked_26(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new PolPnt());
+    }
+
+    private async void Button_Clicked_27(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new RadBstrdSwrd());
+    }
+
+    private async void Button_Clicked_28(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new RinSwrd());
+    }
+
+    private async void Button_Clicked_29(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new SwrdoAsc());
+    }
+
+    private async void Button_Clicked_30(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new UltOmegaSwrd());
+    }
+
+    private async void Button_Clicked_31(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new UltimateSwrdHeavens());
+    }
+
+    private async void Button_Clicked_32(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new VoidcastFalchion());
+    }
+
+    private async void Button_Clicked_33(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new VoidvesselBstrdSwrd());
+    }
+
+    private async void Button_Clicked_34(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new WindsweptShamshir());
+    }
+}
